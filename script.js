f21db0a31a643f5ef67be556937039551dfe0539
Paul-dere7 <139231215+Paul-dere7@users.noreply.github.com>
2024-06-11 12:19:19 +0300

    added html,css and js

---
+++ script.js
@@ -0,0 +1,8 @@
+const typed =new typed('.multipl-text',{
+    strings:['frontend developer','digital marketer','Graphics Design'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+});
